02ab09a67f636e0aa7d9cd1575866e0e135a47cb
Guilherme Staciaki <gstaciaki99@gmail.com>
2025-03-07 00:39:36 -0300

    remove coverage on default tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  // setupFilesAfterEnv: ["./src/setupTests.ts"],
-  // globalSetup: "src/tests/test-utils",
-  collectCoverage: true,
 };
